6bd279c44867e7c70acc7e8a2fcb3412db4d025a
ryorich <ryorich@protonmail.com>
2021-05-09 22:59:55 +0200

    edit fantom pools

--- src/features/configure/vault/fantom_pools.js
+++ src/features/configure/vault/fantom_pools.js
@@ -4,11 +4,11 @@ export const fantomPools = [
     logo: 'busd-pairs/USDC-BUSD.svg', // TODO: change
     name: 'WFTM-USDC LP',
     token: 'WFTM-USDC LP',
-    tokenDescription: 'Spiritswap',
+    tokenDescription: 'SpiritSwap',
     tokenAddress: '0xe7E90f5a767406efF87Fdad7EB07ef407922EC1D',
     tokenDecimals: 18,
     tokenDescriptionUrl: '#',
-    earnedToken: 'WFTM-USDC LP',
+    earnedToken: 'SEANCE_WFTM-USDC',
     earnedTokenAddress: '0x48bdeECB1B4b3a0a6012bffFa24285D2aA85CF56',
     earnContractAddress: '0x48bdeECB1B4b3a0a6012bffFa24285D2aA85CF56',
     pricePerFullShare: 1,
@@ -20,7 +20,7 @@ export const fantomPools = [
     status: 'active',
     platform: 'Spirit',
     assets: ['FTM', 'USDC'],
-    callFee: 0.5,
+    callFee: 0,
     addLiquidityUrl:
       'https://swap.spiritswap.finance/#/add/0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83/0x04068DA6C83AFCFA0e13ba15A6696662335D5B75',
     buyTokenUrl: 'https://swap.spiritswap.finance/#/swap',
