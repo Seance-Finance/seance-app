a4b9e3f574f3e4e13ac94843294d8bc4412ee675
0xIvan <0xivanivan@gmail.com>
2021-05-16 23:59:46 +0200

    Remove disclaimer

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -1,13 +1,6 @@
 import React from 'react';
-
 import Pools from 'features/vault/components/Pools/Pools';
-import Disclaimer from 'components/Disclaimer/Disclaimer';
 
 export default function HomePage() {
-  return (
-    <>
-      <Disclaimer />
-      <Pools fromPage="home" />
-    </>
-  );
+  return <Pools fromPage="home" />;
 }
